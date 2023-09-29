6a334b80d5250b5dbe3a1adcb19ba63e1af7b0e0
Prathwik <64214685+prathwik0@users.noreply.github.com>
2023-09-29 17:06:08 +0530

    fix: year selector in /team
    - Use prop instead of svelte stores
    - Move code away from component and into +page.svelte

--- src/routes/team/+page.svelte
+++ src/routes/team/+page.svelte
@@ -1,5 +1,40 @@
-<script>
-    import TeamPage from "$lib/components/PageWrappers/TeamPage.svelte";
+<script lang="ts">
+	import MemberCardGrid from '$lib/components/CardGrids/MemberCardGrid.svelte';
+	let year = '2023';
 </script>
 
-<TeamPage />
+<div>
+	<div class={'flex w-full justify-center md:space-x-16 space-x-8 font-bold py-10'}>
+		<button
+			class={`md:text-2xl text-xl ${
+				year === '2020' ? 'underline underline-offset-4' : 'no-underline'
+			}`}
+			on:click={() => {
+				year = '2020';
+			}}
+		>
+			2021-22
+		</button>
+		<button
+			class={`md:text-2xl text-xl ${
+				year === '2022' ? 'underline underline-offset-4' : 'no-underline'
+			}`}
+			on:click={() => {
+				year = '2022';
+			}}
+		>
+			2022-23
+		</button>
+		<button
+			class={`md:text-2xl text-xl ${
+				year === '2023' ? 'underline underline-offset-4' : 'no-underline'
+			}`}
+			on:click={() => {
+				year = '2023';
+			}}
+		>
+			2023-24
+		</button>
+	</div>
+	<MemberCardGrid {year} />
+</div>
